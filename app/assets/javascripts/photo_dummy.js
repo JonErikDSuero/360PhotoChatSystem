2f14c0a1b3c0f3c7a322f6d0358b14f8f78bb293
JonErikDSuero <jonerikdsuero@gmail.com>
2015-02-13 11:18:14 -0500

    included panorama

---
+++ app/assets/javascripts/photo_dummy.js
@@ -0,0 +1,11 @@
+var div = document.getElementById('container');
+var PSV = new PhotoSphereViewer({
+  panorama: '/assets/sun.jpg',
+  container: div,
+  time_anim: 3000,
+  navbar: true,
+  navbar_style: {
+    backgroundColor: 'rgba(58, 67, 77, 0.7)'
+  }
+});
+
