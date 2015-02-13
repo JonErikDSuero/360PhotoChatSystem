379da5c162d840c7e4dac3e69a52d2a56207666d
JonErikDSuero <jonerikdsuero@gmail.com>
2015-02-13 15:53:59 -0500

    bring back secrets

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,8 +3,8 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   #provider :developer unless Rails.env.production?
 
   provider :google_oauth2,
-    "995906368455-qvaefh019sbutj0qq20q5bv6pcoovo8u.apps.googleusercontent.com",
-    Rails.application.secrets.google_oauth2
+    Rails.application.secrets.google_oauth2_client_id,
+    Rails.application.secrets.google_oauth2_secret
   {
     scope: [
       "https://www.googleapis.com/auth/drive.file",
