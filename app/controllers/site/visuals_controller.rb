ceadb76eb4188d986e3bd3ef1db0084e02e5c845
JonErikDSuero <jonerikdsuero@gmail.com>
2015-02-15 21:10:49 -0500

    fix syntax error

--- app/controllers/site/visuals_controller.rb
+++ app/controllers/site/visuals_controller.rb
@@ -2,7 +2,7 @@ class Site::VisualsController < ApplicationController
 
   def googledrive
     puts params
-    @current_account = Account.find_by(type: "google", user_id: JSON.parse(params[:state])["userId"]).first
+    @current_account = Account.find_by(type: "google", user_id: JSON.parse(params[:state])["userId"])
     @current_account.api = Api::Google.new(@current_account)
     filenames = @current_account.api.download_files!(params["state"]["ids"])
     @image_url = view_context.image_path(filenames[0]) # assume 1 file for now
