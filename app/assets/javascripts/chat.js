16863885e426f03aeeb154e39b035414d9ae6177
JonErikDSuero <jonerikdsuero@gmail.com>
2015-02-18 15:37:27 -0500

    Play Button

--- app/assets/javascripts/chat.js
+++ app/assets/javascripts/chat.js
@@ -52,7 +52,7 @@ $('body').on('click', '#messages blockquote', function(){
 
 var duration = 2000;
 
-function anim(p1, p2){
+function anim(p1, p2, done){
   var start = null;
   function step(timestamp) {
     if (!start){
@@ -64,6 +64,10 @@ function anim(p1, p2){
     PSV.zoom(new_coord[2]);
     if (progress < duration) {
       window.requestAnimationFrame(step);
+    } else {
+      if (done && typeof done === "function"){
+        done();
+      }
     }
   }
 
@@ -79,4 +83,34 @@ function calc_coord(p1, p2, progress){
 
   return result;
 }
+
+
+$('body').on('click', '#chatroom #play-btn', function(){
+
+  var p1 = [PSV.getPhi(), PSV.getTheta(), PSV.getZoomLevel()]
+  var p2;
+  var messages = $("#messages blockquote");
+  var size = $("#messages blockquote").size();
+  var index = 0;
+
+  recurse();
+
+  function recurse(){
+    if (index != 0){
+      p1 = p2;
+    }
+    phi = $(messages[index]).data("phi");
+    theta = $(messages[index]).data("theta");
+    zoomLevel = $(messages[index]).data("zoomLevel");
+    p2 = [phi, theta, zoomLevel];
+
+    console.log(index);
+    index++;
+    if (size >= index){
+      anim(p1, p2, recurse);
+    }
+  }
+
+});
+
 
